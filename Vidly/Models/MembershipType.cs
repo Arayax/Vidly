1ec6dee6f86a6ae39fa35df3980c2c543bfa6b91
Johan <araya8025@gmail.com>
2020-03-20 23:51:19 -0600

    Campo Name
    crear campo Name en MembershipType

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -8,6 +8,7 @@ namespace Vidly.Models
     public class MembershipType
     {
         public Byte Id { get; set; }
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public Byte DurationInMonths { get; set; }
         public Byte DiscountRate { get; set; }
